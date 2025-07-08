8c564dcddef691aa8d7794d51d205d671f67c55b
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-07-08 16:42:11 +0300

    adding exercise 1 solution in loops exercises

---
+++ Javascript-and-algorithms/Javascript-basics/exercises-Loops/index.js
@@ -0,0 +1,13 @@
+// Loops
+
+
+// Exercise 1
+
+const names = ["Ashley", "Donovan", "Lucas"]
+const ages = [23, 47, 18]
+const people = []
+
+for (let i = 0; i < names.length; i++) {
+    people.push({ name: names[i], age: ages[i] });
+}
+console.log(people);
