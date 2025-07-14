852fb7565551d71a4d9f20e504e713e5521e71a7
SamerAb1 <Samerabuhaya@gmail.com>
2025-07-14 21:39:33 +0300

    uploading contact manager exercise structure

---
+++ Javascript-and-algorithms/error-handling/contact-manager/utils/fileUtils.js
@@ -0,0 +1,7 @@
+// fileUtils.js
+const fs = require('fs');
+
+function readContacts(filePath) { /* ... */ }
+function saveContacts(filePath, contacts) { /* ... */ }
+
+module.exports = { readContacts, saveContacts };
