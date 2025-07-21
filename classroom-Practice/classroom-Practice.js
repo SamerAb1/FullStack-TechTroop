822a7fce0d007a22bd562029bcbf0e44967e9905
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-07-21 14:39:20 +0300

    uploading trees exercises 1-3 solutions

--- classroom-Practice/classroom-Practice.js
+++ classroom-Practice/classroom-Practice.js
@@ -1,29 +1,3 @@
-class BSNode {
-    constructor(value) {
-        this.value = value;
-        this.leftChild;
-        this.rightChild;
-    }
-
-    insertNode(newVal) {
-        if (!this.value) {
-            this.value = newVal
-        }
-        else if (newVal > this.value && this.rightChild) {
-            this.rightChild.insertNode(newVal)
-        }
-        else if(newVal <= this.value && this.leftChild) {
-            this.leftChild.insertNode(newVal)
-        }
-        else if (newVal <= this.value) {
-            this.leftChild = new BSNode(newVal)
-        }
-        else {
-            this.rightChild = new BSNode(newVal)
-        }
-    }
-
-}
 
 const bsN = new BSNode();
 const letters = ["H", "E","S", "G", "L", "Y", "I"];
