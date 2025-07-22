722f8fb6488903bd557af3eaf70b66a0338e3e5d
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-07-22 16:22:08 +0300

    uploading autoCompleteTries class to the js file

--- Javascript-and-algorithms/trie-project/autoCompleteTrie.js
+++ Javascript-and-algorithms/trie-project/autoCompleteTrie.js
@@ -9,11 +9,11 @@ class AutoCompleteTrie{
         if (typeof word !== 'string' || !/^[a-zA-Z]+$/i.test(word)) return;
         word = word.toLowerCase();
         let node = this;
-        for(const c of word){
-            if(!this.children[c]){
-                node.children[c] = new AutoCompleteTrie(c);
+        for(const ch of word){
+            if(!this.children[ch]){
+                node.children[ch] = new AutoCompleteTrie(ch);
             }
-            node = node.children[c];
+            node = node.children[ch];
         }
         node.endOfWord = true;
     }
@@ -22,20 +22,38 @@ class AutoCompleteTrie{
         if (typeof word !== 'string' || !/^[a-zA-Z]+$/i.test(word)) return false;
         word = word.toLowerCase();
         let node = this;
-        for(const c of word){
-            if(!this.children[c]){
-                return false;
-            }
-            node = node.children[c];
+        for(const ch of word){
+            node = node.children[ch];
+            if(!node) return false;
         }
-        if(node.endOfWord == true){
-            return true;
-        }
-        return false;
-
+        return node.endOfWord;
     }
 
     predictWords(prefix){
+        if (typeof prefix !== 'string' || !/^[a-z]+$/i.test(prefix)) return false;
+        prefix = prefix.toLowerCase();
 
+        const start = this._getRemainingTree(prefix,this);
+        if(!start) return false;
+        const words = [];
+        this._allWordsHelper(prefix, start, words);
+        return words;
+    }
+
+    _getRemainingTree(prefix, node) {
+        for (const ch of prefix) {
+        node = node.children[ch];
+        if (!node) return null;
+        }
+        return node;
+    }
+
+    _allWordsHelper(prefix, node, words){
+        if(node.endOfWord) words.push(prefix);
+        for(const [ch, child] of Object.entries(node.children)) {
+            this._allWordsHelper(prefix + ch, child, words);
+        }
     }
 }
+
+module.exports = AutoCompleteTrie;
