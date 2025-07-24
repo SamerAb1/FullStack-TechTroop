57ef6c580e2e739598fd1c1b882a63e3feef47bc
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-07-24 19:10:29 +0300

    adding exercise4

--- JavaScript-and-WebDevelopment/dom-and-events/exercise4/main.js
+++ JavaScript-and-WebDevelopment/dom-and-events/exercise4/main.js
@@ -1,21 +1,66 @@
-const container = document.getElementById("container");
+const nameInput = document.getElementById("name");
+const salaryInput = document.getElementById("salary");
+const birthdayInput = document.getElementById("birthday");
+const phoneInput = document.getElementById("phone");
+const submitBtn = document.getElementById("submit-btn");
+const output = Object.assign(document.createElement("div"), { id: "output" });
 
-for (let i = 0; i < 10; i++) {
-  const box = document.createElement("div");
-  box.className = "box";
-
-  box.onmouseenter = () => {
-    box.style.backgroundColor = getRandomColor();
-  };
-
-  container.appendChild(box);
+function isOnlyLetters(str) {
+  return /^[a-zA-Z]+$/.test(str);
+}
+function isOnlyNumbers(str) {
+  return /^[0-9]+$/.test(str);
 }
 
-function getRandomColor() {
-  return (
-    "#" +
-    Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, "0")
-  );
-}
+const checkName = (name) => {
+  return isOnlyLetters(name) && name.length > 1;
+};
+
+const checkSalary = (salary) => {
+  let intSalary = parseInt(salary);
+  return intSalary > 9999 && intSalary < 16001;
+};
+
+const checkBirthday = (birthday) => {
+  return birthday && birthday.trim() !== "";
+};
+
+const checkPhone = (phone) => {
+  return isOnlyNumbers(phone) && phone.length === 10;
+};
+
+const setOutput = () => {
+  output.style.color = "red";
+  output.style.margin = "16px 0 0 30px";
+  output.style.fontFamily = "'Montserrat', Arial, sans-serif";
+};
+const validateInputs = function () {
+  const name = nameInput.value;
+  const salary = salaryInput.value;
+  const birthday = birthdayInput.value;
+  const phone = phoneInput.value;
+  if (checkName(name)) {
+    if (checkSalary(salary)) {
+      if (checkBirthday(birthday)) {
+        if (checkPhone(phone)) {
+          output.textContent = "Thank You!";
+        } else {
+          output.textContent = "Invalid phone";
+        }
+      } else {
+        output.textContent = "Invalid Birthday";
+      }
+    } else {
+      output.textContent = "Invalid salary";
+    }
+  } else {
+    output.textContent = "Invalid Name";
+  }
+};
+
+submitBtn.onclick = (event) => {
+  event.preventDefault();
+  validateInputs();
+  setOutput();
+  document.getElementById("bottom-row").appendChild(output);
+};
