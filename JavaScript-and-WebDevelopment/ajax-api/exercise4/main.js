9ce7aecccc8233555e7b1e7e823a035a7ea0f2eb
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-07-30 14:48:28 +0300

    adding the solutions of the exercises 4+5 of ajax api

---
+++ JavaScript-and-WebDevelopment/ajax-api/exercise4/main.js
@@ -0,0 +1,21 @@
+const fetch = function (queryValue, apiKey) {
+  $.ajax({
+    method: "GET",
+    url: `https://api.giphy.com/v1/gifs/search?q=${queryValue}&api_key=${apiKey}`,
+    success: function (result) {
+      if (result.data && result.data.length > 0) {
+        let url = result.data[1].embed_url;
+        console.log(result);
+
+        // Create and append an <img> element to the body
+        const gif = $("<iframe>").attr("src", url);
+        $("body").append(gif);
+      }
+    },
+    error: function (xhr, text, error) {
+      console.log(text);
+    },
+  });
+};
+
+fetch("cats", "YsniGN809rUtHa2RHSy7z3Rz87tVYVaZ");
---
+++ JavaScript-and-WebDevelopment/ajax-api/exercise5/main.js
@@ -0,0 +1,30 @@
+const apiKey = "YsniGN809rUtHa2RHSy7z3Rz87tVYVaZ";
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const container = document.getElementById("gif-container");
+const fetch = function (queryValue, apiKey) {
+  $.ajax({
+    method: "GET",
+    url: `https://api.giphy.com/v1/gifs/search?q=${queryValue}&api_key=${apiKey}`,
+    success: function (result) {
+      if (result.data && result.data.length > 0) {
+        const randomIndex = Math.floor(Math.random() * result.data.length);
+        let url = result.data[randomIndex].embed_url;
+        console.log(result);
+
+        // Clear previous content
+        $("#gif-container").empty();
+        // add a new gif to the container
+        const gif = $("<iframe>").attr("src", url);
+        $("#gif-container").append(gif);
+      }
+    },
+    error: function (xhr, text, error) {
+      console.log(text);
+    },
+  });
+};
+searchBtn.onclick = () => {
+  const query = input.value.trim();
+  if (query) fetch(query, apiKey);
+};
