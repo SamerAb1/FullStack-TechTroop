91527e9833fa3af2f3ccc49b9bfb9f45801607c3
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-08-01 23:25:53 +0300

    adding User class to user.js with all the data i need for the UI, +rearranged the main.js

--- JavaScript-and-WebDevelopment/rupg-project/main.js
+++ JavaScript-and-WebDevelopment/rupg-project/main.js
@@ -1,64 +1,55 @@
+import { User } from "./user.js";
+
 const generateBtn = document.getElementById("generate-btn");
 
-async function fetchJson(url) {
-  const response = await fetch(url);
-  if (!response.ok) throw new Error(`Network response was not ok for: ${url}`);
-  return response.json();
-}
-
 async function generateUserData() {
-  const usersApi = "https://randomuser.me/api/?results=7";
-  let data = await fetchJson(usersApi);
-  let firstName = data.results[0].name.first;
-  let lastName = data.results[0].name.last;
-  let location =
-    data.results[0].location.street.name + ", " + data.results[0].location.city;
-  let imgUrl = data.results[0].picture.medium;
-  let friendsArr = data.results.slice(1);
-  let friends = friendsArr.map(
-    (friend) => friend.name.first + " " + friend.name.last
-  );
-
-  console.log("Name:", firstName + " " + lastName);
-  console.log("Location:", location);
-  console.log("Friends:", friends);
-  console.log("Image:", imgUrl);
-}
-
-async function generateQuoteData() {
-  const quoteApi = "https://api.kanye.rest";
-  let data = await fetchJson(quoteApi);
-  console.log("Quote:", data.quote);
-}
-
-async function generatePokemonData() {
-  const randomId = Math.floor(Math.random() * 1025) + 1;
-  const PokemonApi = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
-  let data = await fetchJson(PokemonApi);
-
-  let pokemonName =
-    data.name.charAt(0).toUpperCase() + data.name.slice(1).toLowerCase();
-  let imgUrl = data.sprites.front_default;
-  console.log("Pokemon Name:", pokemonName);
-  console.log("Pokemon Image URL:", imgUrl);
-}
-
-async function generateIpsumData() {
-  const ipsumApi =
-    "https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1";
-  let data = await fetchJson(ipsumApi);
-  console.log("Bacon Ipsum:", data);
-}
-
-function generateData() {
   try {
-    generateUserData();
-    generatePokemonData();
-    generateQuoteData();
-    generateIpsumData();
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
+    // Fetch 7 random users
+    const usersResp = await fetch("https://randomuser.me/api/?results=7");
+    const usersData = await usersResp.json();
+    const main = usersData.results[0];
+    const userName = `${main.name.first} ${main.name.last}`;
+    const userLocation = `${main.location.street.name}, ${main.location.city}`;
+    const userFriends = usersData.results
+      .slice(1)
+      .map((f) => `${f.name.first} ${f.name.last}`);
+
+    // Fetch Kanye quote
+    const quoteResp = await fetch("https://api.kanye.rest");
+    const quoteData = await quoteResp.json();
+    const quote = quoteData.quote;
+
+    // Fetch Pokémon
+    const randomId = Math.floor(Math.random() * 1025) + 1;
+    const pokeResp = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${randomId}`
+    );
+    const pokeData = await pokeResp.json();
+    const pokemonObj = {
+      name:
+        pokeData.name.charAt(0).toUpperCase() +
+        pokeData.name.slice(1).toLowerCase(),
+      imgUrl: pokeData.sprites.front_default,
+    };
+
+    // Fetch Bacon Ipsum
+    const ipsumResp = await fetch(
+      "https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1"
+    );
+    const ipsumData = await ipsumResp.json();
+    const aboutMe = ipsumData.join(" ");
+
+    // Create user and fill all fields
+    const user = new User(userName, userLocation, userFriends);
+    user.setQuote(quote);
+    user.setPokemon(pokemonObj);
+    user.setAboutMe(aboutMe);
+
+    console.log(user);
+  } catch (err) {
+    console.error("Error generating user data:", err.message);
+    alert("There was a problem loading user data. Please try again.");
   }
 }
 
-generateBtn.addEventListener("click", generateData);
+generateBtn.addEventListener("click", generateUserData);
---
+++ JavaScript-and-WebDevelopment/rupg-project/user.js
@@ -0,0 +1,76 @@
+export class User {
+  constructor(name, location, friends) {
+    this.name = name;
+    this.location = location;
+    this.friends = friends;
+    this.quote = "";
+    this.pokemon = {};
+    this.aboutMe = "";
+  }
+
+  setName(name) {
+    if (name) {
+      this.name = name;
+      return true;
+    }
+    return false;
+  }
+  getName() {
+    return this.name;
+  }
+
+  setLocation(location) {
+    if (location) {
+      this.location = location;
+      return true;
+    }
+    return false;
+  }
+  getLocation() {
+    return this.location;
+  }
+
+  setFriends(friends) {
+    if (Array.isArray(friends)) {
+      this.friends = friends;
+      return true;
+    }
+    return false;
+  }
+  getFriends() {
+    return this.friends;
+  }
+
+  setQuote(quote) {
+    if (quote) {
+      this.quote = quote;
+      return true;
+    }
+    return false;
+  }
+  getQuote() {
+    return this.quote;
+  }
+
+  setPokemon(pokemon) {
+    if (pokemon && pokemon.name && pokemon.imgUrl) {
+      this.pokemon = pokemon;
+      return true;
+    }
+    return false;
+  }
+  getPokemon() {
+    return this.pokemon;
+  }
+
+  setAboutMe(aboutMe) {
+    if (aboutMe) {
+      this.aboutMe = aboutMe;
+      return true;
+    }
+    return false;
+  }
+  getAboutMe() {
+    return this.aboutMe;
+  }
+}
