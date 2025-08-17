759e98375af3d8eac9e00c21b36541f301c10947
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-08-17 11:03:42 +0300

    clearing code

--- movies-app/src/auth/AuthProvider.jsx
+++ movies-app/src/auth/AuthProvider.jsx
@@ -1,11 +1,21 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { useState } from "react";
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-  const [activeUser, setActiveUser] = useState(null);
+  const [activeUser, setActiveUser] = useState(
+    JSON.parse(localStorage.activeUser)
+  );
   const navigate = useNavigate();
+
+  useEffect(() => {
+    //Write to localStorage on each change of active user
+    if (localStorage.activeUser !== activeUser) {
+      localStorage.setItem("activeUser", JSON.stringify(activeUser));
+    }
+  }, [activeUser]);
+
   const handleLogin = (email, password) => {
     setTimeout(() => {
       setActiveUser("john");
