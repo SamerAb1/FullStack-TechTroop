49ad18c428ae22904ebf763c4bfa787baf17a62c
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-08-31 14:54:16 +0300

    Express intro and routes exercise files

--- backend/express-intro-routes/server.js
+++ backend/express-intro-routes/server.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const data = {
+  8112: {
+    title: "Name of the Wind",
+    author: "Patrick Rothfuss",
+  },
+  9121: {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+  },
+  1081: {
+    title: "The Giver",
+    author: "Lois Lowry",
+  },
+};
 
-app.get("/", function (request, response) {
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "node_modules")));
+
+app.get("/", function (req, res) {
   console.log("Someone has come into the server. Brace yourselves.");
+  res.send("Ending the cycle, thanks for visiting");
 });
 
+app.get("books/:booksID", function (req, res) {
+  const bookID = req.params.booksID;
+  res.send(data[bookID]);
+});
 const port = 3000;
 app.listen(port, function () {
   console.log(`Running server on port ${port}`);
---
+++ backend/furniture-store/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 3000;
+app.listen(port, function () {
+  console.log(`Server is up and running smoothly`);
+});
