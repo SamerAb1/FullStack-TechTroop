91985aec2f2db81a790221e672fc3dc12cb664ef
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-09-01 19:38:56 +0300

    exercise2 solution

--- backend/express-crud-middleware/server.js
+++ backend/express-crud-middleware/server.js
@@ -4,8 +4,20 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-router.get("/sanity", function (req, res) {
+const wordCounter = [{ word: "cat", count: 2 }];
+
+app.get("/sanity", function (req, res) {
   res.send("Server is up and running");
+});
+
+app.get("/sanity/:word", function (req, res) {
+  const word = req.params.word;
+  const found = wordCounter.find((w) => w.word === word);
+  if (found) {
+    res.send({ count: found.count });
+  } else {
+    res.send({ count: 0 });
+  }
 });
 
 const port = 3000;
